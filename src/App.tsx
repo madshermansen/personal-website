1649c0c5728a53ee63c99fb1d0aef5de97ee97d0
Mads Hermansen <mads.d.hermansen+main@gmail.com>
2023-10-03 01:30:45 +0900

    setup vite + react project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Mads Hermansen</h1>
+      <div className="card">
+        <p>
+          Work in progress
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
