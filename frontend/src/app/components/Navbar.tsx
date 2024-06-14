07290754ba596e14da6999ba0c8587d54ebc3c60
Mads Hermansen <mads.d.hermansen+main@gmail.com>
2024-06-14 18:37:49 +0200

    switch logic to framer motion

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <div>
-            <nav className="flex justify-between items-center p-4 w-full z-50 text-white fixed font-thin">
-                <div className="text-xl hover:text-accent duration-75">Mads Hermansen</div>
+            <nav className="flex justify-between items-center p-4 w-full z-50 text-white fixed font-thin px-8">
+                <div className="text-xl hover:text-accent duration-75 hover:scale-150 cursor-pointer">金羝威</div>
                 <div className="flex space-x-4">
                     <Link href="/" className="hover:text-accent duration-75">Home</Link>
                     <Link href="/projects" className="hover:text-accent duration-75">Projects</Link>
--- frontend/src/app/components/SlidingBackground.tsx
+++ frontend/src/app/components/SlidingBackground.tsx
@@ -1,36 +1,40 @@
 "use client";
-import { useEffect, useRef } from "react";
-
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 export default function SlidingBackground() {
+  const x = useMotionValue(0);
+  const width = useTransform(x, (latestX) =>
+    Math.max(0, Math.min(latestX, window.innerWidth))
+  );
 
-  const leftSideRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const leftSide = leftSideRef.current;
-
-    if (!leftSide) return;
-
-    const handleMouseMove = (e: MouseEvent) => {
-      const p = (e.clientX / window.innerWidth) * 100;
-      console.log(p);
-      leftSide.style.width = `${p}%`;
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []); 
+  const handleMouseMove: React.MouseEventHandler<HTMLElement> = (event) => {
+    x.set(event.clientX);
+  };
 
   return (
-    <main>
-      <div ref={leftSideRef} className="grid h-screen overflow-hidden place-items-center fixed w-full z-10 bg-background">
-        <h1 className="text-6vw leading-tight m-10-screen w-80vw text-center font-bold">Making something great should take <span className="fancy text-primary font-normal">Effort</span></h1>
-      </div>
-      <div className="grid h-screen overflow-hidden place-items-center fixed w-full bg-primary">
-        <h1 className="text-6vw leading-tight m-10-screen w-80vw text-center text-background font-bold">Making something great should take <span className="fancy text-white font-normal">Skill</span></h1>
+    <main onMouseMove={handleMouseMove} className="relative">
+      {/* Second Div (Foreground, Initially Hidden) */}
+      <motion.div
+        className="grid h-screen overflow-hidden place-items-center fixed w-full bg-background select-none z-10"
+        style={{
+          width,
+          overflow: "hidden",
+        }}
+      >
+        <h1 className="text-6vw leading-tight m-10-screen w-80vw text-center font-bold">
+          Making something great should take{" "}
+          <span className="fancy text-primary font-normal">Effort</span>
+        </h1>
+      </motion.div>
+
+      {/* First Div (Background) */}
+      <div
+        className="grid h-screen overflow-hidden place-items-center fixed w-full bg-primary"
+      >
+        <h1 className="text-6vw leading-tight m-10-screen w-80vw text-center text-background font-bold">
+          Making something great should take{" "}
+          <span className="fancy text-white font-normal">Skill</span>
+        </h1>
       </div>
     </main>
   );
