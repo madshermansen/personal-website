5b1e30ef75a3ddb6edc42cc82b3ac4fd11738fa4
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-10-13 14:40:19 +0900

    feat(sanity): add projectType schema for projects

---
+++ frontend/src/sanity/schemaTypes/projectType.ts
@@ -0,0 +1,37 @@
+export const projectType = {
+  title: "Project",
+  name: "project",
+  type: "document",
+  fields: [
+    {
+      title: "Title",
+      name: "title",
+      type: "string",
+    },
+    {
+      title: "Description",
+      name: "description",
+      type: "text",
+    },
+    {
+      title: "GitHub URL",
+      name: "github",
+      type: "url",
+    },
+    {
+      title: "Blog Text",
+      name: "blog",
+      type: "text",
+    },
+    {
+      title: "Demo URL",
+      name: "url",
+      type: "url",
+    },
+    {
+      title: "Image",
+      name: "image",
+      type: "image",
+    },
+  ],
+};
