be54a8a3f70316a75d03aa45c402538f795b8aca
Mads Hermansen <119772939+madshermansen@users.noreply.github.com>
2024-10-17 12:32:40 +0900

    add project page to display project details in a table format

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -1,25 +1,61 @@
-"use client"
+"use client";
 import useProjects from "@/lib/hooks/useProjects";
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 export default function Projects() {
+
   const { projectData, loading } = useProjects();
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <main>
       <h1>Projects</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Tags</th>
+            <th>Github URL</th>
+            <th>Demo</th>
+          </tr>
+        </thead>
+        <tbody>
           {projectData.projects.map((project) => (
-            <li key={project.slug.current}>
-              <h2>{project.title}</h2>
-              <p>{project.description}</p>
-            </li>
+            <tr
+              key={project.slug.current}
+            >
+              <td className="hover:text-accent font-bold"><Link href={
+                `/projects/${project.slug.current}`
+              }>{project.title}</Link></td>
+              <td>{project.description}</td>
+              <td>
+                {project.tags &&
+                  project.tags.map((tag: string) => (
+                    <span key={tag}>
+                      {tag}
+                    </span>
+                  ))}
+              </td>
+              <td>
+                <Link href={project.github}>
+                  <FaGithub />
+                </Link>
+              </td>
+              <td>
+                {project.url && (
+                  <Link href={project.url}>
+                    Demo
+                  </Link>
+                )}
+              </td>
+            </tr>
           ))}
-        </ul>
-      )}
-
+        </tbody>
+      </table>
     </main>
   );
 }
